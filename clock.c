b5abdbb2ca7dca89185bb5f67f25bc44fb9ba2f7
Shubhamoy Ghosh <shubhamoyghosh2006@gmail.com>
2026-07-02 21:41:29 +0530

    Add files via upload

---
+++ clock.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(){
+ 
+    int hour=0, minute=0, second=0;
+    hour=minute=second;
+    while(1){
+
+        system("clear");
+
+        printf("%02d : %02d : %02d",hour,minute,second);
+
+    fflush(stdout);
+    second ++ ;
+
+    if(second==60){
+        minute+=1;
+        second=0;
+    }
+    if(minute==60){
+        hour+=1;
+        minute=0; 
+    }if(hour==24){
+        hour=0;
+        minute=0;
+        second=0;
+    }
+    sleep(1);
+
+    }
+
+getchar(); // pause
+getchar(); // waits for Enter
+return 0;
+}
